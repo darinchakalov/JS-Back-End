404d2cff1bbb882c01a4809a294c0c41f7a55c11
darinchakalov <darinchakalov@hotmail.com>
2022-02-26 08:51:05 +0200

    db config

--- Regular Exam/src/config/constants.js
+++ Regular Exam/src/config/constants.js
@@ -1,5 +1,6 @@
 const DB_PASS = "realestatepass";
+const DB_NAME = "Exam-DB";
 exports.PORT = "3000";
-exports.DB_STRING = `mongodb+srv://realestateuser:${DB_PASS}@cluster0.ozv6x.mongodb.net/videoTutorials?retryWrites=true&w=majority`;
+exports.DB_STRING = `mongodb+srv://realestateuser:${DB_PASS}@cluster0.ozv6x.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 exports.SECRET = "mEG5sDv4b5i5BtC37MbAxGu6W5gRWeKR";
 exports.TOKEN_COOKIE_NAME = "app_token";
