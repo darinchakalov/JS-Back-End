3b924ae12e54a28ef45dff6624298ea4546b4ba7
darinchakalov <darinchakalov@hotmail.com>
2022-02-26 07:37:22 +0200

    sceleton

--- Real Estate Agency/config/db-config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const initDB = function () {
-	return mongoose.connect(
-		"mongodb+srv://realestateuser:realestatepass@cluster0.ozv6x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-	);
-};
-
-module.exports = initDB;
--- Real Estate Agency/config/routes.js
+++
@@ -1,7 +0,0 @@
-const homeController = require("../controllers/homeController.js");
-const authController = require("../controllers/authController.js");
-
-module.exports = (app) => {
-	app.use(homeController);
-	app.use(authController);
-};
